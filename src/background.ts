1feec72a8a2a6337dc01ee05a0e6dfb3346447f7
yuji96 <y.yama683@gmail.com>
2023-10-28 23:40:23 +0900

    cors で post できない

--- src/background.ts
+++ src/background.ts
@@ -3,6 +3,7 @@ import { BibLatexParser } from "biblatex-csl-converter";
 console.log("Hello from background.ts");
 
 // TODO: url で action の enabled を切り替えたい
+// TODO: async/await にしたい
 
 chrome.action.onClicked.addListener((tab) => {
   console.log("clicked");
@@ -12,15 +13,14 @@ chrome.action.onClicked.addListener((tab) => {
   console.log("send message");
   chrome.tabs.sendMessage(tab.id, { message: "hello" }).then((bibtex) => {
     console.log(bibtex);
-    // let parser = new BibLatexParser(bibtex, {processUnexpected: true, processUnknown: true})
     const parser = new BibLatexParser(bibtex);
     const parsed = parser.parse();
-    const info = parsed.entries[1].fields;
+    const info: any = parsed.entries[1].fields;
 
     // "family, given" -> "given family"
-    const author_reversed = info.author.map(
-      (name) => `${name.given[0].text} ${name.family[0].text}`
-    );
+    const author_reversed: string = info.author
+      .map((name: any) => `${name.given[0].text} ${name.family[0].text}`)
+      .join(", ");
 
     chrome.tabs.query({ active: true, lastFocusedWindow: true }).then((tabs) => {
       console.log(
@@ -31,6 +31,22 @@ chrome.action.onClicked.addListener((tab) => {
         info.url,
         tabs[0].url
       );
+
+      const notion = new Client({ auth: NOTION_TOKEN });
+      (async () =>
+        await notion.pages.create({
+          parent: { database_id: DATABASE_ID! },
+          properties: {
+            Title: { title: [{ text: { content: "title" } }] },
+            "Author(s)": {
+              rich_text: [{ type: "text", text: { content: "authors" } }],
+            },
+            Booktitle: { rich_text: [{ type: "text", text: { content: "book" } }] },
+            Year: { number: 2021 },
+            URL: { url: "url" },
+            PDF: { url: "pdf" },
+          },
+        }))();
     });
   });
 });
