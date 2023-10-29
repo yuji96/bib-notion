2e4bf33967745a0dc4dcd323e7c5d254b07765fc
yuji96 <y.yama683@gmail.com>
2023-10-29 16:44:58 +0900

    bootstrap が適用された

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+// Import our custom CSS
+import "../scss/styles.scss";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// or, specify which plugins you need:
+// import { Tooltip, Toast, Popover } from 'bootstrap'; // noqa
+
